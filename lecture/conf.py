6b7c961519ac2d3249f5e08e844dc754c5cd2863
PeerHerholz <herholz.peer@gmail.com>
2021-10-28 10:04:31 +0200

    update configuration files

--- lecture/conf.py
+++ lecture/conf.py
@@ -52,7 +52,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 #
 html_title = ""
 html_theme = "sphinx_book_theme"
-html_logo = "_static/gorella_logo.png"
+html_logo = "_static/pfp_logo.png"
 html_theme_options = {
     "github_url": "https://github.com/PeerHerholz/Python_for_Psychologists_Winter2021",
     "repository_url": "https://github.com/PeerHerholz/Python_for_Psychologists_Winter2021",
